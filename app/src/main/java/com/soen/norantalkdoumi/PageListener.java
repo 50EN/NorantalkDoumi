538a33b84ccede9f849edfaab10a9fe59f44ccb7
pororili <xiozioll@gmail.com>
2019-11-22 10:20:14 +0900

    0.0.1 BaseActivity, PageListener completed. TODO : LockActivity, lockenc DEADLINE : this saturday

---
+++ app/src/main/java/com/soen/norantalkdoumi/PageListener.java
@@ -0,0 +1,13 @@
+package com.soen.norantalkdoumi;
+
+import android.app.Activity;
+
+public interface PageListener {
+    void onActivityCreated(Activity activity);
+    void onActivityStarted(Activity activity);
+    void onActivityResumed(Activity activity);
+    void onActivityPaused(Activity activity);
+    void onActivityStopped(Activity activity);
+    void onActivitySaveInstanceState(Activity activity);
+    void onActivityDestroyed(Activity activity);
+}
