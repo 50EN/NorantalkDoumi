0ce229187d4ee60a88a02179a2f989a8998b4d22
Nevaland <deanh0716@gmail.com>
2019-11-30 18:23:49 +0900

    0.5.2 MainActivity, menu buttons

--- app/src/main/java/com/soen/norantalkdoumi/MainActivity.java
+++ app/src/main/java/com/soen/norantalkdoumi/MainActivity.java
@@ -2,11 +2,13 @@ package com.soen.norantalkdoumi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends BaseActivity{
-    public static final int LOCK_REQUEST_CODE = 1001;// Lock Activity Code (Maybe Need to Change)
-    public static final int RESERV_REQUEST_CODE = 1002;// Reserv Activity Code (Maybe Need to Change)
-    public static final int SETTING_REQUEST_CODE = 1003;// Setting Activity Code (Maybe Need to Change)
+    public static final int LOCK_REQUEST_CODE = 1001;       // Lock Activity Code (Maybe Need to Change)
+    public static final int RESERV_REQUEST_CODE = 1002;     // Reserv Activity Code (Maybe Need to Change)
+    public static final int SETTING_REQUEST_CODE = 1003;    // Setting Activity Code (Maybe Need to Change)
     public static String password = "";             // Temp User Password
     DataBase database = new DataBase();
 
@@ -18,14 +20,33 @@ public class MainActivity extends BaseActivity{
 //     DataBase.DbOpenHelper.DbHelper.create();
         //sqliteDB = init_database();
 
-
         // Get User Password in DB
-        password = ""; // Need to Add DB Function
+        password = "9af15b336e6a9619928537df30b2e6a2376569fcf9d7e773eccede65606529a0"; // Need to Add DB Function
         // Call Lock Activity
         Intent intent = new Intent(getApplicationContext(), com.soen.norantalkdoumi.LockActivity.class);
         intent.putExtra("passwd", password);            // Transfer Password by Parameter (name:passwd)
         startActivityForResult(intent, LOCK_REQUEST_CODE);
+
+        ImageButton btnRecord = (ImageButton)findViewById(R.id.Record);
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Call Reservation Activity
+                Intent intentReserv = new Intent(getApplicationContext(), com.soen.norantalkdoumi.ReservActivity.class);
+                startActivityForResult(intentReserv, RESERV_REQUEST_CODE);
+            }
+        });
+        ImageButton btnSetting = (ImageButton)findViewById(R.id.Setting);
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Call Setting Activity
+                Intent intentSetting = new Intent(getApplicationContext(), com.soen.norantalkdoumi.SettingActivity.class);
+                startActivityForResult(intentSetting, SETTING_REQUEST_CODE);
+            }
+        });
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
